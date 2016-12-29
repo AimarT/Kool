c340aa4923f283d5d901e1cda15568495dfd8d5d
AimarT <Aimar.Tamm@tptlive.ee>
2016-12-29 19:57:03 +0200

    OK

--- src/C8EX4IndexofMax.java
+++ src/C8EX4IndexofMax.java
@@ -2,8 +2,18 @@
  * Created by admin on 27.12.2016.
  */
 public class C8EX4IndexofMax {
-    public static void  IndexofMax (int [] m )  {
-
+    public static int  IndexofMax (int [] m, int  n )  {
+        for (int i = 0; i < m.length; i++) {
+            if (m [i] > n  ) {
+                return i;
+            }
+        }
+        return -1;
     }
 
+    public static void main(String[] args) {
+        int [] m = new int[1];
+        System.out.println(IndexofMax(m,24));
+        System.out.println(IndexofMax(m,7));
+    }
 }
---
+++ src/C8EX6.java
@@ -0,0 +1,8 @@
+/**
+ * Created by admin on 29.12.2016.
+ */
+public class C8EX6 {
+    public static int [] areFactors (int n){
+        int [] a = new int[n];
+    }
+}
